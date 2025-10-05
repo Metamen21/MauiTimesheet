ec92f1de4adb09ddc2f56ca38c3dba792d1badbe
Metamen21 <60350995+Metamen21@users.noreply.github.com>
2025-10-06 00:02:35 +0530

    Add project files.

---
+++ MauiTimesheet/MauiInterop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiTimesheet
+{
+    internal class MauiInterop
+    {
+        public static async Task AlertAsync(string message,string title)=> 
+                         App.Current!.Windows[0].Page!.DisplayAlert(title, message, "OK");
+
+    }
+}
---
+++ MauiTimesheet/Models/RegisterModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MauiTimesheet.Models;
+
+public class RegisterModel
+{
+    [Required]
+    public string FullName { get; set; }
+    [Required]
+    public string UserName { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
